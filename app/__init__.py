e6274919e8efd3b29bcc0b60421e37d3b1782398
Oleg A <t0rr@mail.ru>
2022-01-16 18:39:52 +0300

    feat: add flasgger docs

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,35 @@
+from flasgger import Swagger
 from flask import Flask
 from flask.logging import create_logger
 
 from .core.alchemy import init_alchemy
 
 app = Flask(__name__)
+swagger = Swagger(app)
 logger = create_logger(app)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route("/health")
+def root_handler():
+    """Check app health
+    ---
+    tags:
+      - utils
+    produces:
+      - application/json
+    schemes: ['http', 'https']
+    definitions:
+      BoolAnswer:
+        type: object
+        properties:
+          success:
+            type: boolean
+    responses:
+      200:
+        schema:
+          $ref: '#/definitions/BoolAnswer'
+    """
+    return {"success": True}
 
 
 @app.before_first_request
