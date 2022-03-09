1dbcbf76e0a90be3db09ec4acf7762a756ca1363
DmitryK <kachkindmitry@gmail.com>
2022-03-09 21:52:43 +0300

    GET and POST reqests , postgres ping removed

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -5,7 +5,7 @@ from typing import Any, Dict, Optional, Union
 import aioredis
 import asyncpg
 import backoff
-import pytest
+import pytest_asyncio
 from aiohttp import ClientSession
 from aioredis import Redis
 from asyncpg import Connection
@@ -23,12 +23,12 @@ class HTTPResponse(BaseModel):
     status: int
 
 
-@pytest.fixture(name="settings", scope="session")
+@pytest_asyncio.fixture(name="settings", scope="session")
 def settings_fixture() -> TestSettings:
     return TestSettings()
 
 
-@pytest.fixture(name="event_loop", scope="session")
+@pytest_asyncio.fixture(name="event_loop", scope="session")
 def event_loop_fixture() -> asyncio.AbstractEventLoop:
     """Create an instance of the default event loop."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
@@ -36,27 +36,49 @@ def event_loop_fixture() -> asyncio.AbstractEventLoop:
     loop.close()
 
 
-@pytest.fixture(name="http_client", scope="session")
-async def http_client_fixture(settings, postgres_client, redis_client) -> ClientSession:
+@pytest_asyncio.fixture(name="redis_client", scope="session")
+async def redis_client_fixture(settings: TestSettings) -> Redis:
+    redis = aioredis.from_url(
+        f"redis://{settings.redis_host}:{settings.redis_port}",
+        encoding="utf8",
+        decode_responses=True,
+    )
+    await wait_for_ping(redis, settings)
+    yield redis
+    await redis.flushall()
+    await redis.close()
+
+
+@pytest_asyncio.fixture(name="http_client", scope="session")
+async def http_client_fixture(settings, redis_client) -> ClientSession:
     """Represents HTTP client fixture.
 
     Add dependency fixtures `postgres_client` and `redis_client` to
     check they are ready to work.
     """
-    async with ClientSession(base_url=settings.pytest_settings.test_url) as session:
+    async with ClientSession(base_url=settings.test_url) as session:
         yield session
 
 
-@pytest.fixture(name="make_get_request")
-def make_get_request_fixture(http_client):
+@pytest_asyncio.fixture(name="make_request")
+def make_request_fixture(http_client):
     """Make HTTP-request"""
 
-    async def inner(url: str, params: Optional[Dict[str, Any]] = None) -> HTTPResponse:
+    async def inner(method: str, url: str,
+                    params: Optional[Dict[str, Any]] = None,
+                    json: Optional[Dict[str, Any]] = None) -> HTTPResponse:
         params = params or {}
         url = f"/auth/{url}"
         logger.debug("URL: %s", url)
 
-        async with http_client.get(url, params=params) as response:
+        req = http_client
+        if method == "GET":
+            req = req.get
+
+        if method == "POST":
+            req = req.post
+
+        async with req(url, params=params, json=json) as response:
             body = await response.json()
             logger.warning("Response: %s", body)
 
@@ -69,43 +91,27 @@ def make_get_request_fixture(http_client):
     return inner
 
 
-@pytest.fixture(name="redis_client", scope="session")
-async def redis_client_fixture(settings: TestSettings) -> Redis:
-    redis = aioredis.from_url(
-        f"redis://{settings.redis_settings.host}:{settings.redis_settings.port}",
-        encoding="utf8",
-        decode_responses=True,
-    )
-    await wait_for_ping(redis, settings)
-    yield redis
-    await redis.flushall()
-    await redis.close()
-
-
-@pytest.fixture(name="postgres_client", scope="session")
+@pytest_asyncio.fixture(name="postgres_client", scope="session")
 async def postgres_client_fixture(settings: TestSettings):
-    pg_settings = settings.postgres_settings
     conn = await asyncpg.connect(
-        user=pg_settings.username,
-        password=pg_settings.password,
-        host=pg_settings.host,
-        port=pg_settings.port,
-        dbname=pg_settings.dbname,
+        user=settings.postgres_username,
+        password=settings.postgres_password,
+        host=settings.postgres_host,
+        port=settings.postgres_port
     )
-    await wait_for_ping(conn, settings)
     yield conn
-    await conn.execute("DROP TABLE %s", pg_settings.dbname)
+    await conn.execute("DROP TABLE %s", settings.postgres_dbname)
     await conn.close()
 
 
 async def wait_for_ping(client: Union[Redis, Connection], settings: TestSettings):
-    """Wait for service client will answer to ping."""
+    """Wait for service client to answer """
     client_name = type(client).__name__
 
     @backoff.on_exception(
         wait_gen=backoff.expo,
         exception=(RuntimeError, ConnectionError, TimeoutError),
-        max_time=settings.pytest_settings.ping_backoff_timeout,
+        max_time=settings.ping_backoff_timeout,
     )
     async def _ping(inner_client):
         result = await inner_client.ping()
