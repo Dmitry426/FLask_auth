68bd4954948de77a990e52772354aa5bc9faa5e7
DmitryK <kachkindmitry@gmail.com>
2022-03-10 22:21:06 +0300

    Auth route tests including /history and /change

--- tests/functional/src/auth_test.py
+++ tests/functional/src/auth_test.py
@@ -6,12 +6,60 @@ import pytest
 logger = logging.getLogger(__name__)
 
 user = {"login": "Test", "password": "QWERTy90!"}
+
+access_token = {}
+refresh_token = {}
+
+change_name = {"login": "Best", "password": "QWERTysds90!"}
+
 pytestmark = pytest.mark.asyncio
 
 
 async def test_create_user(make_request):
     response = await make_request(method="POST", url="registration", json=user)
     assert response.status == HTTPStatus.CREATED
-    logger.debug("Response status : %s", response.status)
-    assert response.body
+    logger.info("Response status : %s", response.status)
+    assert response.body["result"] == "User successfully created"
     logger.info("Response status : %s", response.body)
+
+
+async def test_login_user(make_request):
+    response = await make_request(method="POST", url="login", json=user)
+    assert response.status == HTTPStatus.OK
+    access_token["access_token"] = response.body["access_token"]
+    refresh_token["refresh_token"] = response.body["refresh_token"]
+    logger.info("Response status : %s", response.status)
+
+
+async def test_refresh_token(make_request):
+    response = await make_request(method="POST", url="refresh", json=refresh_token)
+    assert response.status == HTTPStatus.OK
+    access_token["access_token"] = response.body["access_token"]
+    refresh_token["refresh_token"] = response.body["refresh_token"]
+    logger.info("Response status : %s", response.status)
+
+
+async def test_change_login(make_request):
+    response = await make_request(
+        method="POST", url="change", json=change_name, jwt=access_token["access_token"]
+    )
+    assert response.status == HTTPStatus.ACCEPTED
+    logger.info("Response status : %s", response.status)
+
+
+async def test_history(make_request):
+    response = await make_request(
+        method="GET", url="history", jwt=access_token["access_token"]
+    )
+
+    assert response.status == HTTPStatus.OK
+    logger.info("Response status : %s", response.status)
+
+
+async def test_logout(make_request):
+    response = await make_request(
+        method="POST", url="logout", jwt=access_token["access_token"]
+    )
+
+    assert response.status == HTTPStatus.CREATED
+    logger.info("Response status : %s", response.status)
